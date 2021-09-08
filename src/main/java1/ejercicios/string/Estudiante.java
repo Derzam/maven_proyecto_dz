ea8a71f0127dbbfbddfe3cd26181a6646a37b38d
Derzam <derlymoreira192@gmail.com>
2021-09-08 16:52:29 -0500

    Deber

---
+++ src/main/java1/ejercicios/string/Estudiante.java
@@ -0,0 +1,26 @@
+package ejercicios.string;
+
+public class Estudiante extends MainEstudiante {
+	
+	String GuardadoCedula;
+	String cedula;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getGuardadoCedula() {
+		return GuardadoCedula;
+	}
+
+	public void setGuardadoCedula(String guardadoCedula) {
+		GuardadoCedula = guardadoCedula;
+	} 
+	
+	
+
+}
