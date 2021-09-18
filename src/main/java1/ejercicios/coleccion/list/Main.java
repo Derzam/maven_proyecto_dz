6eccb97a9a5594ff872071fd57022ea5bb33a0a9
Derzam <derlymoreira192@gmail.com>
2021-09-17 20:12:07 -0500

    EJERCICIOS LIST

--- src/main/java1/ejercicios/coleccion/list/Main.java
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -1,9 +1,27 @@
 package ejercicios.coleccion.list;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Integer  numero1=1;
+		Integer  numero2=2;
+		Integer  numero3=3;
+		Integer  numero4=4;
+		Integer  numero5=5;
+		Integer  numero6=6;
+		
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
+		listaNueva.add(numero5);
+		listaNueva.add(numero6);
+		
+		int vector [] = new int [10];
 
 	}
 
