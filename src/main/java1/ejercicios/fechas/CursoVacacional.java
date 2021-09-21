61fee8b6039d3a585d45fa7087b7c2a6aa784433
Derzam <derlymoreira192@gmail.com>
2021-09-20 19:36:52 -0500

    TALLER

--- src/main/java1/ejercicios/fechas/CursoVacacional.java
+++ src/main/java1/ejercicios/fechas/CursoVacacional.java
@@ -1,5 +1,50 @@
 package ejercicios.fechas;
 
+import java.time.LocalDate;
+
 public class CursoVacacional {
+	private String diaHoy;
+	private String diaFin;
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	public String getDiaHoy() {
+		return diaHoy;
+	}
+	public void setDiaHoy(String diaHoy) {
+		this.diaHoy = diaHoy;
+	}
+	public String getDiaFin() {
+		return diaFin;
+	}
+	public void setDiaFin(String diaFin) {
+		this.diaFin = diaFin;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(LocalDate diaHoy2) {
+		this.fechaInicio = diaHoy2;
+	}
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	
 
 }
--- src/main/java1/ejercicios/fechas/Main.java
+++ src/main/java1/ejercicios/fechas/Main.java
@@ -1,10 +1,41 @@
 package ejercicios.fechas;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Date date = new Date();
+		
+		LocalDate diaHoy = LocalDate.now();
+		LocalDate diaFin = diaHoy.plusDays(15);
+		
+		CursoVacacional curso1 = new CursoVacacional();
+		curso1.setNombre("Volley Principiantes");
+		curso1.setFechaInicio(diaHoy);
+		curso1.setFechaFin(diaFin);
+		
+		System.out.println("Nombre: " +curso1.getNombre());
+		System.out.println("F I: " + curso1.getFechaInicio());
+		System.out.println("F F: " + curso1.getFechaFin());
+		
+		LocalDate diaHoy2 = LocalDate.now();
+		LocalDate diaQueInicio = diaHoy2.minusDays(2);
+		LocalDate diaQueFinaliza = diaQueInicio.plusDays(20);
+		
+		CursoVacacional curso2 = new CursoVacacional();
+		curso2.setNombre("Volley Principiantes");
+		curso2.setFechaInicio(diaHoy);
+		curso2.setFechaFin(diaFin);
+		
+		System.out.println("Nombre: " +curso2.getNombre());
+		System.out.println("F I: " + curso2.getFechaInicio());
+		System.out.println("F F: " + curso2.getFechaFin());
+		
+		
 	}
 
 }
